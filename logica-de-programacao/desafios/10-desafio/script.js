56a6dec4a47125aa548278d99f3082fb2efdd06c
jnralexandre <alexandre.o.a.jr.estudos@gmail.com>
2024-06-06 08:55:31 -0300

    Add: exercise 10

---
+++ logica-de-programacao/desafios/10-desafio/script.js
@@ -0,0 +1,10 @@
+let idadeNecessariaParaTirarHabilitacao = 18;
+
+let idadeDoUsuario = prompt("Por favor, informe sua idade para continuar!");
+
+if (idadeDoUsuario >= idadeNecessariaParaTirarHabilitacao) {
+    alert("Pode tirar habilitação!");
+} else {
+    alert("Você não pode tirar habilitação!");
+}
+
