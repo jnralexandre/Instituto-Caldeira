3e1b8ff8172579c8308efc895a89cbddad37ceae
jnralexandre <alexandre.o.a.jr.estudos@gmail.com>
2024-06-06 13:37:40 -0300

    Add: exercise 03

---
+++ logica-de-programacao/03-lista-de-desafios/03-desafio/script.js
@@ -0,0 +1,10 @@
+console.log("Início!");
+
+let contador = prompt("Por informe um número para continuar: ");
+
+while (contador > 0) {
+    console.log(contador);
+    contador -= 1;
+}
+
+console.log("Fim!")
