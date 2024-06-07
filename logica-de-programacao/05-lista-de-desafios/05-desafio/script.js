a92db4374bd42490e4c0dce1b321febe4baa6592
jnralexandre <alexandre.o.a.jr.estudos@gmail.com>
2024-06-06 23:33:43 -0300

    Add: exercise 05

---
+++ logica-de-programacao/05-lista-de-desafios/05-desafio/script.js
@@ -0,0 +1,8 @@
+function calcular() {
+    let valor1 = parseInt(prompt("Por favor, informe um número inteiro: "));
+    let valor2 = parseInt(prompt("Por fim, informe mais um número inteiro: "));
+
+    let resultado = (valor1 + valor2);
+
+    alert(`O resultado é: ${resultado}`);
+}
