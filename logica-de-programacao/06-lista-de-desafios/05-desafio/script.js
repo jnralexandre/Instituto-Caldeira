c856bf028f388d831acd6f27d7373e14f73a47fe
jnralexandre <alexandre.o.a.jr.estudos@gmail.com>
2024-06-07 10:13:24 -0300

    refactor: - adds: semicolin

--- logica-de-programacao/06-lista-de-desafios/05-desafio/script.js
+++ logica-de-programacao/06-lista-de-desafios/05-desafio/script.js
@@ -2,9 +2,9 @@ retornarOMaiorValor(9, 12);
 
 function retornarOMaiorValor(valor1, valor2) {
     if (valor1 > valor2) {
-        return console.log(`O valor: ${valor1} é maior que: ${valor2}`)
+        return console.log(`O valor: ${valor1} é maior que: ${valor2}`);
     } else if (valor2 > valor1) {
-        return console.log(`O valor: ${valor2} é maior que: ${valor1}`)
+        return console.log(`O valor: ${valor2} é maior que: ${valor1}`);
     }
 
     return console.log(`Os valores são iguais`);
