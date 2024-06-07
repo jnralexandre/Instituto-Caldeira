4b3ec20bd43350bc20c02fcb098b7afdf74f0565
jnralexandre <alexandre.o.a.jr.estudos@gmail.com>
2024-06-07 10:11:02 -0300

    Add: exercise 06

---
+++ logica-de-programacao/06-lista-de-desafios/06-desafio/script.js
@@ -0,0 +1,7 @@
+elevarAoQuadrado(9);
+
+function elevarAoQuadrado(valor1) {
+    let multiplicar = (valor1 * valor1);
+
+    return console.log(`O número ${valor1} elevado ao quadrado é: ${multiplicar}`);
+}
