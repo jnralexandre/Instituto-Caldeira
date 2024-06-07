f4997885319a874031b77b3c12e418d7f4241919
jnralexandre <alexandre.o.a.jr.estudos@gmail.com>
2024-06-07 14:10:55 -0300

    Add: exercise 03

---
+++ logica-de-programacao/07-lista-de-desafios/03-desafio/script.js
@@ -0,0 +1,7 @@
+converterDolarParaReal(5)
+
+function converterDolarParaReal(valor) {
+    let reais = (valor * 4.80);
+
+    return console.log(`A conversão de $${valor} USD para BRLR É R$${reais}`);
+}
